e47cdae19aa8aa76efb9f1be39eb7a6f1ea925a2
Dmytro Pustovit <dmytro.pustovit@gmail.com>
2022-07-10 00:30:28 +0300

    Chore: ignore sign errors

--- bin/sign-firefox.sh
+++ bin/sign-firefox.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 npm run build:firefox
-npx web-ext sign -s build/firefox -a build -v --api-key $FIREFOX_API_KEY --api-secret $FIREFOX_SECRET_KEY --channel listed
+npx web-ext sign -s build/firefox -a build -v --api-key $FIREFOX_API_KEY --api-secret $FIREFOX_SECRET_KEY --channel listed || true
